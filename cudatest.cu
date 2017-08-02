f956b4b6fe7703d7bc4f9ebb2eab126f6107cc37
izham-sugita <30282614+izham-sugita@users.noreply.github.com>
2017-08-02 20:05:27 +0900

    Code clean-up.
    Clean up comments.

--- cudatest.cu
+++ cudatest.cu
@@ -4,15 +4,11 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
-//for cublas function
-#include<cublas_v2.h>
-
 // simple kernel function that adds two vectors
+// originally used for demonstration
 __global__ void vect_add(float *a, float *b, int N)
-//__global__ void vect_add(float *a, float *b, ulong N)
 {
    int idx = threadIdx.x;
-   //ulong idx = threadIdx.x;
    if (idx<N) a[idx] = a[idx] + b[idx];
 }
 
@@ -71,56 +67,3 @@ cudaFree(d_b);
 printf("Test passed!\n");
 
 }
-
-// function called from main fortran program
-/*
-extern "C" void kernel_wrapper_(float *a, float *b, int *Np)
-{
-   float  *a_d, *b_d;  // declare GPU vector copies
-   
-   int blocks = 1;     // uses 1 block of
-   int N = *Np;        // N threads on GPU
-   //N = N/blocks;
-
-//ulong blocks = 2;
-//ulong N = *Np;
-//N = N/(blocks+1);
-
-   // Allocate memory on GPU
-   cudaMalloc( (void **)&a_d, sizeof(float) * N );
-   cudaMalloc( (void **)&b_d, sizeof(float) * N );
-
-   // copy vectors from CPU to GPU
-   cudaMemcpy( a_d, a, sizeof(float) * N, cudaMemcpyHostToDevice );
-   cudaMemcpy( b_d, b, sizeof(float) * N, cudaMemcpyHostToDevice );
-
-   // call function on GPU
-   //vect_add<<< blocks, N >>>( a_d, b_d, N);
-   vect_add<<< blocks, N >>>( a_d, b_d, N);
-
-   // copy vectors back from GPU to CPU
-   cudaMemcpy( a, a_d, sizeof(float) * N, cudaMemcpyDeviceToHost );
-   //cudaMemcpy( b, b_d, sizeof(float) * N, cudaMemcpyDeviceToHost );
-
-   // free GPU memory
-   cudaFree(a_d);
-   cudaFree(b_d);
-   return;
-}*/
-
-
-/*
-extern "C" void kernel_wrapper_cublas_saxpy(float* A, float* B, int size_A)
-{
-
-checkCudaErrors(cudaMalloc((void **) &d_A, mem_size_A));
-
- cublasHandle_t handle;
- checkCudaErrors(cublasCreate(&handle));
- float alpha = 1.0;
- int incx =1;
- int incy =1;
- checkCudaErrors(cublasSaxpy(handle, size_A, &alpha, d_A, incx, d_B, incy )); 
-
-}
-*/
